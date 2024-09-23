ca515391fbc558b5a8c73f8284fb0913da960140
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-23 21:27:10 +0300

    Account Model Added

--- Lesson_01/Program.cs
+++ Lesson_01/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Bank App");
+
+
+
+Account account1 = new();
+account1.FirstName = "John";
+account1.LastName = "Doe";
+account1.PhoneNumber = "123456789";
+account1.IBAN = "TR123456789";
+account1.Balance = 1000;
+
+account1.Withdraw(750);
+account1.Withdraw(300);
+account1.Withdraw(10000);
+
+Console.WriteLine($"Balance: {account1.Balance}");
+
+class Account
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string IBAN { get; set; }
+    public decimal Balance { get; set; } //1000
+
+    public void Withdraw(decimal amount) //1500
+    {
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
