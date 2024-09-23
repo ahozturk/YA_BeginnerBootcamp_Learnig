ddcb855a78f5eba49ca834bc1b36b064a2215a23
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-23 21:37:11 +0300

    SetBalance and ShowBalance Features Added

--- Lesson_01/Program.cs
+++ Lesson_01/Program.cs
@@ -1,19 +1,18 @@
 ﻿Console.WriteLine("Bank App");
 
-
-
 Account account1 = new();
 account1.FirstName = "John";
 account1.LastName = "Doe";
 account1.PhoneNumber = "123456789";
 account1.IBAN = "TR123456789";
-account1.Balance = 1000;
+account1.SetBalance(1000);
 
 account1.Withdraw(750);
 account1.Withdraw(300);
 account1.Withdraw(10000);
 
-Console.WriteLine($"Balance: {account1.Balance}");
+account1.ShowBalance();
+// Console.WriteLine($"Balance: {account1.Balance}");
 
 class Account
 {
@@ -21,7 +20,18 @@ class Account
     public string LastName { get; set; }
     public string PhoneNumber { get; set; }
     public string IBAN { get; set; }
-    public decimal Balance { get; set; } //1000
+    private decimal Balance { get; set; } //1000
+
+    public void SetBalance(decimal balance)
+    {
+        if (balance <= 1000)
+            Balance = balance;
+    }
+
+    public void ShowBalance()
+    {
+        Console.WriteLine($"Balance: {Balance}");
+    }
 
     public void Withdraw(decimal amount) //1500
     {
