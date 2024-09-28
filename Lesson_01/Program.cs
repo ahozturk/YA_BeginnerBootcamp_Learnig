aaddd66e3b181d94a4096ee5b9650de242a94b99
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-28 14:26:55 +0300

    Withdraw Method Updated
    - Withdraw login changed

--- Lesson_01/Program.cs
+++ Lesson_01/Program.cs
@@ -1,18 +1,20 @@
 ﻿Console.WriteLine("Bank App");
 
-Account account1 = new();
+Account account1 = new(400);
 account1.FirstName = "John";
 account1.LastName = "Doe";
 account1.PhoneNumber = "123456789";
 account1.IBAN = "TR123456789";
-account1.SetBalance(1000);
 
 account1.Withdraw(750);
 account1.Withdraw(300);
-account1.Withdraw(10000);
+// account1.Withdraw(10000);
 
-account1.ShowBalance();
-// Console.WriteLine($"Balance: {account1.Balance}");
+
+account1.Withdraw(750);
+System.Console.WriteLine();
+
+Console.WriteLine($"Balance: {account1.GetBalance()}");
 
 class Account
 {
@@ -22,22 +24,49 @@ class Account
     public string IBAN { get; set; }
     private decimal Balance { get; set; } //1000
 
-    public void SetBalance(decimal balance)
+    //15
+    //15/1
+
+    public Account()
+    {
+        Balance = 100;
+    }
+
+    public Account(decimal balance)
     {
         if (balance <= 1000)
             Balance = balance;
     }
 
-    public void ShowBalance()
+    // public void SetBalance(decimal balance)
+    // {
+    //     if (balance <= 1000)
+    //         Balance = balance;
+    // }
+
+    public decimal GetBalance() //Geri dönüş tipi Decimal olan bir metot ile değiştirilecek
     {
-        Console.WriteLine($"Balance: {Balance}");
+        return Balance;
     }
 
     public void Withdraw(decimal amount) //1500
     {
+        //Balance: 1000
+        //Amount: 1500
         if (Balance >= amount)
         {
             Balance -= amount;
         }
+        else if (Balance != 0)
+        {
+            System.Console.WriteLine($"Insufficient balance but you withdraw this amount: {Balance}");
+            Balance = 0;
+        }
+        else
+        {
+            System.Console.WriteLine("Insufficient balance");
+        }
+        //End Balance: 1000
+        
     }
 }
