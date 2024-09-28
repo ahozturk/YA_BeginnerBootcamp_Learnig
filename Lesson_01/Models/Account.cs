48f597e578bca7ded66b9a16ee89c28fb7fd38c1
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-28 15:00:17 +0300

    Transfer Money Method Added

---
+++ Lesson_01/Models/Account.cs
@@ -0,0 +1,64 @@
+namespace Lesson_01.Models;
+
+public class Account
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string IBAN { get; set; }
+    private decimal Balance { get; set; } //1000
+
+    //15
+    //15/1
+
+    public Account()
+    {
+        Balance = 100;
+    }
+
+    public Account(decimal balance)
+    {
+        if (balance <= 1000)
+            Balance = balance;
+    }
+
+    // public void SetBalance(decimal balance)
+    // {
+    //     if (balance <= 1000)
+    //         Balance = balance;
+    // }
+
+    public decimal GetBalance() //Geri dönüş tipi Decimal olan bir metot ile değiştirilecek
+    {
+        return Balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+    public decimal Withdraw(decimal amount) //1500
+    {
+        //Balance: 1000
+        //Amount: 1500
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+            return amount;
+        }
+        else if (Balance != 0)
+        {
+            Console.WriteLine($"Insufficient balance but you withdraw this amount: {Balance}");
+            decimal tempBalance = Balance;
+            Balance = 0;
+            return tempBalance;
+        }
+        else
+        {
+            System.Console.WriteLine("Insufficient balance");
+            return 0;
+        }
+        //End Balance: 1000
+        
+    }
+}
--- Lesson_01/Program.cs
+++ Lesson_01/Program.cs
@@ -1,31 +1,24 @@
-﻿Console.WriteLine("Bank App");
+﻿using Lesson_01.Models;
+
+Console.WriteLine("Bank App");
 
 List<Account> accounts = new();
 
-Account account1 = new(400);
+Account account1 = new(300); //300
 account1.FirstName = "John";
 account1.LastName = "Doe";
 account1.PhoneNumber = "123456789";
 account1.IBAN = "TR123456789";
 accounts.Add(account1);
 
-Account account2 = new();
+Account account2 = new(1000); //1000
 account2.FirstName = "Jane";
 account2.LastName = "Doe";
 account2.PhoneNumber = "987654321";
 account2.IBAN = "TR987654321";
 accounts.Add(account2);
 
-string depositIBAN = "TR987654321";
-decimal depositAmount = 750;
-
-for (int i = 0; i < accounts.Count; i++)
-{
-    if (accounts[i].IBAN == depositIBAN)
-    {
-        accounts[i].Deposit(depositAmount);
-    }
-}
+TransferMoney(account1, account2, 500);
 
 Console.WriteLine($"John's Balance: {account1.GetBalance()}");
 Console.WriteLine($"Jane's Balance: {account2.GetBalance()}");
@@ -37,61 +30,8 @@ Console.WriteLine($"Jane's Balance: {account2.GetBalance()}");
 
 Console.WriteLine($"Balance: {account1.GetBalance()}");
 
-class Account
+void TransferMoney(Account sender, Account receiver, decimal amount)
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string PhoneNumber { get; set; }
-    public string IBAN { get; set; }
-    private decimal Balance { get; set; } //1000
-
-    //15
-    //15/1
-
-    public Account()
-    {
-        Balance = 100;
-    }
-
-    public Account(decimal balance)
-    {
-        if (balance <= 1000)
-            Balance = balance;
-    }
-
-    // public void SetBalance(decimal balance)
-    // {
-    //     if (balance <= 1000)
-    //         Balance = balance;
-    // }
-
-    public decimal GetBalance() //Geri dönüş tipi Decimal olan bir metot ile değiştirilecek
-    {
-        return Balance;
-    }
-
-    public void Deposit(decimal amount)
-    {
-        Balance += amount;
-    }
-    public void Withdraw(decimal amount) //1500
-    {
-        //Balance: 1000
-        //Amount: 1500
-        if (Balance >= amount)
-        {
-            Balance -= amount;
-        }
-        else if (Balance != 0)
-        {
-            System.Console.WriteLine($"Insufficient balance but you withdraw this amount: {Balance}");
-            Balance = 0;
-        }
-        else
-        {
-            System.Console.WriteLine("Insufficient balance");
-        }
-        //End Balance: 1000
-        
-    }
+    decimal sendAmount = sender.Withdraw(amount);
+    receiver.Deposit(sendAmount);
 }
